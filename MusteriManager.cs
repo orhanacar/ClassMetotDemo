29825c46c8dbbcb9599168ce9ab28c34dc2f7192
orhan <orhan@DESKTOP-83EL6NB>
2021-01-20 18:49:21 +0300

    20 ocak 2021 ödev

---
+++ MusteriManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri) 
+        {
+            Console.WriteLine(musteri.MusteriAdi+" "+musteri.MusteriSoyadi+" bilgileri eklenmiştir.");
+        }
+        public void MusteriListeleme(Musteri musteri)
+        {
+            Console.WriteLine(musteri.MusteriAdi + " " + musteri.MusteriSoyadi+" Bilgileri Listelenmiştir.");
+                   
+        }
+        public void MusteriSilme(Musteri musteri)
+        {
+            Console.WriteLine(musteri.MusteriAdi + " " + musteri.MusteriSoyadi + " Bilgileri Silinmiştir.");
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,20 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Musteri musteri1 = new Musteri();
+            musteri1.MusteriAdi = "Orhan";
+            musteri1.MusteriSoyadi = "Acar";
+            musteri1.MusteriTel = "0123456789";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.MusteriAdi = "Ali";
+            musteri2.MusteriSoyadi = "veli";
+            musteri2.MusteriTel = "02469788533";
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.MusteriEkle(musteri1);
+            musteriManager.MusteriEkle(musteri2);
+            musteriManager.MusteriListeleme(musteri1);
+            musteriManager.MusteriSilme(musteri2);
         }
     }
 }
